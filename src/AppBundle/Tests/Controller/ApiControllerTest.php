683e7114d1f0e889af65fe628fd4b6c2d44bd33e
Andrey <moby.ekreative@gmail.com>
2015-02-25 23:23:56 +0200

    change api test

--- src/AppBundle/Tests/Controller/ApiControllerTest.php
+++ src/AppBundle/Tests/Controller/ApiControllerTest.php
@@ -36,7 +36,7 @@ class ApiControllerTest extends WebTestCase
 
     public function setUp()
     {
-        $this->client = static::createClient();
+        $this->client = static::createClient([], ['HTTP-']);
 
         $this->token = "token";
 
@@ -49,27 +49,36 @@ class ApiControllerTest extends WebTestCase
             ->getManager();
     }
 
-    public function testTagsAction()
+    /**
+     * @dataProvider getData
+     */
+    public function testTagsAction($a, $b)
     {
-        $tag = $this->em
-            ->getRepository('AppBundle:Tag')
-            ->findOneBy([]);
+        $this->assertEquals($a,$b);
 
-        $this->client->request('GET', '/api/tags',
-            [], // params
-            [], // files
-            ['HTTP_AUTHORIZATION' => $this->token],
-            null // json_encode()
-        );
-        $response = $this->client->getResponse();
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('application/json', $response->headers->get('content-type'));
-        $json = json_decode($response->getContent(), true);
-        $this->assertInternalType('array', $json);
-        if (count($json)) {
-            $this->assertArrayHasKey('id', $json['0']);
-            $this->assertArrayHasKey('title', $json['0']);
-        }
+//        $tag = $this->em
+//            ->getRepository('AppBundle:Tag')
+//            ->findOneBy([]);
+//
+//        $this->client->request('GET', '/api/tags',
+//            [], // params
+//            [], // files
+//            ['HTTP_AUTHORIZATION' => $this->token],
+//            null // json_encode()
+//        );
+//        $response = $this->client->getResponse();
+//        $this->assertEquals(200, $response->getStatusCode());
+//        $this->assertEquals('application/json', $response->headers->get('content-type'));
+//        $json = json_decode($response->getContent(), true);
+//        $this->assertInternalType('array', $json);
+//        if (count($json)) {
+//            $this->assertArrayHasKey('id', $json['0']);
+//            $this->assertArrayHasKey('title', $json['0']);
+//        }
+    }
+
+    public function getData( ) {
+        return [[0,1], [1,1], [1,0]];
     }
 
     /**
